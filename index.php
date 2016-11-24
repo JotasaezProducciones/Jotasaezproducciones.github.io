b138d11b773f15940bb3051d1d1bacb56d651864
Jotasaez <jotasaez@hotmail.com>
2016-11-24 20:51:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-# Jotasaezproducciones.github.io
+<html>
+    
+<head>
+  
+<meta http-equiv="Refresh" content="0;url=https://jotasaezpro.blogspot.com/">
+</head>
+ <!--<center>
+<a href="https://www.facebook.com/dialog/pagetab?app_id=202337533126237&amp;next=https://apps.facebook.com/joansaezz" target="_blank">
+<span style="font-size: small;">Agregar a Mi Página de Facebook</span></a></center>-->
+
+</html>
