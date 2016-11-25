971ef3f1e152b1291ec524caaef621d52e40d3c1
Jotasaez <jotasaez@hotmail.com>
2016-11-24 21:16:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
     
 <head>
-<!--<meta http-equiv="Refresh" content="0;url=https://jotasaezpro.blogspot.com/">-->
+<meta http-equiv="Refresh" content="0;url=https://jotasaezpro.blogspot.com/">
 </head>
  <!--<center>
 <a href="https://www.facebook.com/dialog/pagetab?app_id=206218566085554&amp;next=https://apps.facebook.com/jotasaezproducciones" target="_blank">
